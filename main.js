fe5ff34063712534f4d6e8a0559cfb913c227c94
vijaydev133 <vijaydeveloper004@gmail.com>
2022-08-09 14:29:28 +0530

    Adding code.

---
+++ main.js
@@ -0,0 +1,213 @@
+let arr = [4,8,6,1,3,7,9,15]
+
+let arr1 = ["y","i","p","t","c"]
+
+let obj = {
+    fName : "Chellak",
+    lName : "Kutty"
+}
+
+
+/******************************************************* */
+//at()
+
+// function atMethod(num){
+//     return arr.at(num)
+// }
+
+// console.log(atMethod(-2));
+// console.log(arr);
+
+/******************************************************* */
+//concat()
+
+// function concatMethod(){
+//     return arr1.concat(arr)
+// }
+
+// console.log(concatMethod());
+// console.log(arr);
+
+/******************************************************* */
+//copyWithin()
+
+// function copyWithinMethod(){
+//     return arr.copyWithin(3,6)
+// }
+
+// console.log(copyWithinMethod());
+
+
+/******************************************************* */
+//entries()
+
+// let iterator  = arr.entries()
+
+// for(el of iterator){
+//     console.log(el);
+// }
+
+/******************************************************* */
+//every()
+
+// console.log(arr.every(el=> el > 4));
+
+
+/******************************************************* */
+//fill()
+
+// arr.fill(4,2)
+
+// console.log(arr);
+
+/******************************************************* */
+//filter()
+
+// console.log(arr.filter(el=> el > 5))
+
+// console.log(arr);
+
+/******************************************************* */
+//find()
+
+// console.log(arr.find(el=> el > 5));
+
+/******************************************************* */
+//findIndex()
+
+// console.log(arr.findIndex(el=> el > 7));
+
+/******************************************************* */
+//flat()
+
+// let array = [[1],2,8,[6,7]]
+
+// console.log(array.flat());
+
+// console.log(array.flatMap(e=> e));
+
+/******************************************************* */
+//forEach()
+
+// arr.forEach(e=>{
+//    console.log(e*2);
+// })
+
+/******************************************************* */
+//Array.from()
+// let Arr = Array.from(arr)
+// console.log(Arr);
+// console.log(arr);
+
+
+/******************************************************* */
+//includes()
+
+// console.log(arr.includes(8));
+
+
+
+/******************************************************* */
+//indexOf()
+
+// console.log(arr.indexOf(15));
+
+/******************************************************* */
+//join()
+
+// console.log(typeof  arr.join(","));
+
+
+/******************************************************* */
+//keys()
+// let arrKeys = arr.keys()
+
+// for(key of arrKeys){
+//     console.log(key);
+// }
+
+/******************************************************* */
+//lastIndexOf()
+
+// console.log(arr.lastIndexOf(8));
+
+
+/******************************************************* */
+//map()
+
+// let map1 = arr.map(e=>e)
+// console.log(map1);
+// map1[0]= 5
+
+// console.log(arr);
+// console.log(map1);
+
+/******************************************************* */
+//Array.of()
+// console.log(Array.of(7));
+// console.log(Array(7));
+
+
+/******************************************************* */
+//pop()
+// console.log(arr.pop());
+//push()
+// console.log(arr.push(25));
+//shift
+// console.log(arr.shift());
+//unshift
+// console.log(arr.unshift(78));
+
+
+/******************************************************* */
+//reduce
+//reduceRight()
+
+// let result = arr.reduce((prev,cur)=>{
+//       return  prev = prev + cur
+// },0)
+
+// console.log(result);
+
+// let res = arr1.reduceRight((prev,curr)=>{
+//  return   prev = prev + curr
+// },"")
+
+// console.log(res);
+
+/******************************************************* */
+//reverse()
+// arr.reverse()
+// console.log(arr);
+
+/******************************************************* */
+//slice()
+
+// console.log(arr.slice(-2));
+// console.log(arr);
+
+/******************************************************* */
+//some()
+
+// console.log(arr.some(el => el > 4));
+
+/********************************************************/
+//sort()
+// arr1.sort()
+// console.log(arr1);
+
+/********************************************************/
+//splice
+
+// let arrrr = arr.splice(1,0,47)
+// console.log(arrrr);
+// console.log(arr);
+
+/********************************************************/
+// values()
+
+// let it = arr.values()
+
+// for(val of it){
+//     console.log(val);
+// }
